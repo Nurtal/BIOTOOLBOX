adb83b711f583c35387af7e3d34864f881c6bb2a
PC_immuno <PC_immuno>
2017-07-27 17:52:22 +0200

    finish trash function

--- trash.py
+++ trash.py
@@ -99,10 +99,15 @@ def detect_file_format(data_file_name):
 def change_file_format(data_file_name, separator):
 	"""
 	-> Change the separator used in data_file_name, to separator
-	=> TODO:
-		- deal with extension in the output file name
-	[IN PROGRESS]
+	-> Delete spaces in lines if space is not the separator 
 	"""
+
+	## Define the extension
+	extension = "_reformated.tmp"
+	if(separator == ","):
+		extension = "_reformated.csv"
+	elif(separator == "\t"):
+		extension = "_reformated.tsv"
 
 	## Get current separator
 	current_separator = detect_file_format(data_file_name)
@@ -111,12 +116,13 @@ def change_file_format(data_file_name, separator):
 		## Re-write file with new separator
 		output_file_name = data_file_name.split(".")
 		output_file_name = output_file_name[0]
-		output_file_name = str(output_file_name)+"_reformated.tmp"
-
+		output_file_name = str(output_file_name)+extension
 		input_data_file = open(data_file_name, "r")
 		output_data_file = open(output_file_name, "w")
 		for line in input_data_file:
 			line_to_write = line.replace(current_separator, separator)
+			if(separator != " "):
+				line_to_write = line_to_write.replace(" ", "")
 			output_data_file.write(line_to_write)
 		output_data_file.close()
 		input_data_file.close()
@@ -219,10 +225,171 @@ def clean():
 
 
 
+def get_description_of_variables(input_file):
+	"""
+	-> Try to figure out if variables describe in input_file are
+	   qualitative or quantitative.
+	-> return a dict with the name of quantitative and qualitative variable
+	"""
+	type_to_variables = {}
+	type_to_variables["qualitative"] = []
+	type_to_variables["quantitative"] = []
+
+	## Get the data 
+	index_to_variable = {}
+	variable_to_value = {}
+	input_data = open(input_file, "r")
+	cmpt = 0
+	for line in input_data:
+		line = line.replace("\n", "")
+		line_in_array = line.split(",")
+		if(cmpt == 0):
+			index = 0
+			for var in line_in_array:
+				index_to_variable[index] = var
+				variable_to_value[var] = []
+				index += 1
+		else:
+			index = 0
+			for scalar in line_in_array:
+				variable_to_value[index_to_variable[index]].append(scalar)
+				index += 1
+		cmpt += 1
+	input_data.close()
+
+	## Test if variable is qualitative or not
+	quantitative_variable = []
+	qualitative_variable = []
+	for key in variable_to_value.keys():
+		var_is_quantitative = True
+		for scalar in variable_to_value[key]:
+			if(scalar != "NA"):
+				try:
+					scalar = float(scalar)
+				except:
+					var_is_quantitative = False
+		if(var_is_quantitative):
+			quantitative_variable.append(key)
+		else:
+			qualitative_variable.append(key)
+	type_to_variables["qualitative"] = qualitative_variable
+	type_to_variables["quantitative"] = quantitative_variable
+
+	## return the results
+	return type_to_variables
+
+
+
+def build_data_file_from(explain_variable, descriptives_variable, data_file):
+	"""
+	-> Build a new file from data_file
+	-> explain_variable is a string, the name of the variable to explain (the first column)
+	-> descriptives_variable is a list of descriptives_variable
+	TODO:
+		- test data position on a small data file
+	"""
+
+	data_file_name = data_file.split(".")
+	data_file_name = data_file_name[0]
+	output_name = "subset_from_"+str(data_file_name)+".csv"
+	index_to_variable = {}
+	variable_to_index = {}
+	index_to_variable_in_new_array = {}
+
+	data = open(data_file, "r")
+	output = open(output_name, "w")
+	cmpt = 0
+
+	## Prepare header
+	header = ""
+	header += explain_variable+","
+	index_to_variable_in_new_array[0] = explain_variable
+	index = 1
+	for var in descriptives_variable:
+		header += var +","
+		index_to_variable_in_new_array[index] = var
+		index +=1
+	header = header[:-1]
+	output.write(header+"\n")
+	for line in data:
+		line = line.replace("\n", "")
+		line_in_array = line.split(",")
+		if(cmpt == 0):
+			index = 0
+			for var in line_in_array:
+				if(explain_variable == var):
+					index_to_variable[explain_variable] = index
+					variable_to_index[var] = index
+				elif(var in descriptives_variable):
+					index_to_variable[var] = index
+					variable_to_index[var] = index
+				index += 1
+		else:
+			line_to_write = ""
+			line_to_write += line_in_array[variable_to_index[explain_variable]]+","
+			for var in descriptives_variable:
+				line_to_write += line_in_array[variable_to_index[var]] + ","
+			line_to_write = line_to_write[:-1]
+			output.write(line_to_write+"\n")
+		cmpt +=1
+	output.close()
+	data.close()
+
+
+def drop_patients_with_NA(input_file):
+	"""
+	-> Create a new file from input_file and
+	   drop every line containing NA values
+	"""
+
+	data_file_name = input_file.split(".")
+	data_file_name = data_file_name[0]
+	output_name = str(data_file_name)+"_without_NA.csv"
+
+	input_data = open(input_file, "r")
+	output_data = open(output_name, "w")
+
+	for line in input_data:
+		line = line.replace("\n", "")
+		line_in_array = line.split(",")
+
+		keep_the_line = True
+
+		if("NA" in line_in_array):
+			keep_the_line = False
+
+		if(keep_the_line):
+			output_data.write(line+"\n")
+
+	output_data.close()
+	input_data.close()
+
+
+def control_subset_file(file_to_test):
+	"""
+	-> count the number of line in a file
+	-> return True if > 10 cases in file, if not return False
+	"""
+
+	data = open(file_to_test, "r")
+	cmpt = 0
+	for line in data:
+		cmpt += 1
+	data.close()
+	enought_patient_in_file = True
+	if(cmpt < 10):
+		enought_patient_in_file = False
+
+	return enought_patient_in_file
+
 
 ### TEST SPACE ###
 #sep = detect_file_format("play.txt")
 #print sep
-#change_file_format("play.txt", ";")
+#change_file_format("clinical_i2b2trans.txt", ",")
 #fix_file_name("test/play.wth.reformated.csv")
-clean()
+#truc = get_description_of_variables("clinical_i2b2trans_reformated.csv")
+#build_data_file_from("\\Clinical\\Lab\\LBHANEMIA", ["\\HLA\\Alleles\\HLADRB10406","\\HLA\\Alleles\\HLAC0202","\\HLA\\Alleles\\HLAA3601"], "clinical_i2b2trans_reformated.csv")
+#drop_patients_with_NA("subset_from_clinical_i2b2trans_reformated.csv")
+#test = control_subset_file("subset_from_clinical_i2b2trans_reformated.csv")
+#clean()
